d66c054bd55d17de5047541725dce71b95fabd9f
flow111817 <3181948507@qq.com>
2026-09-09 21:57:51 +0800

    0.0.2

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ const GITHUB_CONFIG = {
     owner: 'flow111817',
     repo: '0923',
     branch: 'main',
-    encryptedToken: 'G0tH1hTXk1aMo7k9:+LSTjGrX9MEz4+FIOUk+UPM5tCLOdtwZ/YWPxpI9auAczmEWY9xlCH+3WbVY/PXjDP4HoBdeGP/G3vBxdrZwuqwxNEEtd828NJ7I5lxn9+fDaVkPyoS+cecIbkooiolO1yFlEttJKD0uOIsBlA==',
+    encryptedToken: 'vuD7HvtGV6d4Xcsl:NMcvH/zUZ5+QqAfFSYm0pSTlo9bUNL3FGOyILcsr4TchOL7F2hgN+fRVJ9Gc2Bv0VVPvunLQTmA=',
     passwordHash: '5790b2c7bbf52f4338b8422ba3ee13ff4917589ab1f0ec30a20ad15a8014275e'
 };
